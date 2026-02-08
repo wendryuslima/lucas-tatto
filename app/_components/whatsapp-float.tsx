8df3e52e2728562b8f95c9f3e4b6a2e5b5dc04f0
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2026-02-08 17:34:07 -0300

    refactor: add link whats

--- app/_components/whatsapp-float.tsx
+++ app/_components/whatsapp-float.tsx
@@ -10,7 +10,7 @@ const WhatsappFloat = () => {
 
   return (
     <a
-      href="https://wa.me/5511999999999"
+      href="https://wa.me/5511993461896"
       target="_blank"
       rel="noopener noreferrer"
       aria-label={t.whatsapp.ariaLabel}
